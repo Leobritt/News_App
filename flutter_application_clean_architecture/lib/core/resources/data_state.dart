aa93f6a5f238f887b0cb5004de9236a8760d6059
Leobritt <leobritto160@gmail.com>
2023-08-10 15:51:15 -0300

    creation of date state class and article class

---
+++ flutter_application_clean_architecture/lib/core/resources/data_state.dart
@@ -0,0 +1,19 @@
+import 'package:dio/dio.dart';
+
+//class to comunicate wih network
+abstract class DataState<T> {
+  final T? data;
+  final DioError? error;
+//DioError represents the error that occurred during the operation, allowing proper diagnosis and treatment.
+  const DataState({this.data, this.error});
+}
+
+//successful response
+class DataSucess<T> extends DataState<T> {
+  const DataSucess(T data) : super(data: data);
+}
+
+//when erro occurs while sending the request or receiving the response
+class DataFailed<T> extends DataState<T> {
+  const DataFailed(DioError error) : super(error: error);
+}
